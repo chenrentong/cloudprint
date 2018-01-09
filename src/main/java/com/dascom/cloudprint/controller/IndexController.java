0ae6909d66f8de46fe117a33e0691e5670eb212d
DS-035 <DS-035@DS-022>
2018-01-09 17:02:34 +0800

    测试

--- src/main/java/com/dascom/cloudprint/controller/IndexController.java
+++ src/main/java/com/dascom/cloudprint/controller/IndexController.java
@@ -36,13 +36,6 @@ public class IndexController {
 	@RequestMapping("welcome")
 	public String welcome(HttpServletRequest request ){
 		Logg.writeDebugLog("进入欢迎页,welcome");
-		//aaa
-		//撒啊啊
-		//oa2
-		//分
-		//oafenzhi
-		//主该
-
 		return "/welcome";
 	}
 	
